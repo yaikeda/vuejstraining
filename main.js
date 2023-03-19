85a17d3f588c2e8bb8bef69347359691d756a904
yaikeda <yaikeda.io@outlook.com>
2023-03-19 10:45:22 +0900

    [main] Initial files.

---
+++ main.js
@@ -0,0 +1,20 @@
+// Initialize applicattion instance (like a class?)
+var app = new Vue({
+    el: '#app', // id name witch is connected to Vue.js
+    data : {
+        message: "Hello Vue.js!", // put string into message variable
+        list: ['apple', 'banana', 'strawberry'], // list variable
+        number: "-1",
+        show: true,
+        count: 0,
+        radius: 50,
+    },
+    methods :{
+        handleClick: function(event) {
+            alert(event.target)
+        },
+        increment: function() {
+            this.count += 1
+        }
+    }
+})
